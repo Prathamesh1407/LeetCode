801c972259514f0b33600da451bf4052bf1252e5
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-03-10 07:46:30 +0530

    Time: 4 ms (68.61%), Space: 13.1 MB (32.08%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
+        
+        
         
         vector<int>ans;
-        unordered_set<int>s;
+        unordered_map<int,int>m;
         
-        int i=0,j=0;
-        while(i<nums1.size() && j<nums2.size())
+        for(int i=0;i<nums1.size();i++)
         {
-            if(nums1[i]==nums2[j])
-            {
-                s.insert(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i]<nums2[j]) i++;
-            else j++;
+            m[nums1[i]]++;
         }
-        
-        for(auto val:s) ans.push_back(val);
+        for(int i=0;i<nums2.size();i++)
+        {
+            if(m.find(nums2[i])!=m.end())
+            {
+                ans.push_back(nums2[i]);
+                m.erase(nums2[i]);
+            }
+        }
         return ans;
     }
 };
