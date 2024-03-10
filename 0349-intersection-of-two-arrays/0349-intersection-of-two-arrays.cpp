3eb9cb16a255a69f82bae7848bc9c70afac9091f
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-03-10 07:56:21 +0530

    Update 0349-intersection-of-two-arrays.cpp

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -23,3 +23,34 @@ public:
         return ans;
     }
 };
+
+
+//2nd Approach
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        
+        vector<int>ans;
+        unordered_set<int>s;
+        
+        int i=0,j=0;
+        while(i<nums1.size() && j<nums2.size())
+        {
+            if(nums1[i]==nums2[j])
+            {
+                s.insert(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j]) i++;
+            else j++;
+        }
+        
+        for(auto val:s) ans.push_back(val);
+        return ans;
+    }
+};
