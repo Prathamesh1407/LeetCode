6528d1c6a11f95170e61a7728ff1df4d2533f762
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-03-14 16:50:25 +0530

    Time: 73 ms (65.90%), Space: 46.1 MB (9.76%) - LeetHub

---
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        unordered_map<string,int>m,p;
+        int i=0;
+        vector<string>ans;
+        for(auto val:list1) 
+        {
+            m[val]=i;
+            i++;
+        }
+        i=0;
+        for(auto val:list2) 
+        {
+            if(m.find(val)!=m.end())
+            {
+                p[val]=m[val]+i;
+            }
+            i++;
+        }
+        int mini=10000;
+        string s;
+        for(auto val:p)
+        {
+            if(val.second<=mini)
+            {
+                mini=min(mini,val.second);
+            }
+        }
+        for(auto val:p)
+        {
+            if(val.second==mini)
+            {
+                ans.push_back(val.first);  
+            }
+        }
+        return ans;
+        
+    }
+};
