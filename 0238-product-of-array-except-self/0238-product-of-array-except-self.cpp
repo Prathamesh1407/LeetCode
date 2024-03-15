7f5715a7090b38e376770810e5127daf511b48d9
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-03-15 15:31:47 +0530

    Time: 6 ms (98.82%), Space: 26.4 MB (55.36%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,22 +4,18 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         vector<int>ans(nums.size());
-        int premult=1;
-        vector<int>pre(nums.size()+1);
-        vector<int>post(nums.size()+1);
-        pre[0]=1;
-        post[nums.size()]=1;
-        for(int i=0;i<nums.size();i++)
+        ans[0]=1;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++)
         {
-            pre[i+1]=pre[i]*nums[i];
+            ans[i+1]=ans[i]*nums[i];
         }
-        for(int i=nums.size()-1;i>=0;i--)
+        int post=nums[n-1];
+        for(int i=n-2;i>=0;i--)
         {
-            post[i]=post[i+1]*nums[i];
-        }
-        for(int i=1;i<=nums.size();i++)
-        {
-            ans[i-1]=pre[i-1]*post[i];
+            ans[i]=ans[i]*post;
+            post*=nums[i];
+            
         }
         return ans;
     }
