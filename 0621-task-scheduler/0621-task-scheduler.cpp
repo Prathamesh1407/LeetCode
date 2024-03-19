b5e6a4f06f17eb1513414eabd6cac7a97186d867
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-03-19 10:37:21 +0530

    Time: 34 ms (99.08%), Space: 37.9 MB (80.77%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        if(n==0) return tasks.size();
         vector<int>v(26);
         for(int i=0;i<tasks.size();i++)
         {
             v[tasks[i]-'A']++;
         }
-        for(auto val:v) cout<<val<<" ";
+        //for(auto val:v) cout<<val<<" ";
         sort(v.begin(),v.end(),greater<int>());
         int idle_slots=(v[0]-1)*n;
         for(int i=1;i<26;i++)
