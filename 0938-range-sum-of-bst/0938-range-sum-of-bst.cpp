3a96fb069870bb5cd866dd38d3f3d04ca218c6ed
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-03-20 14:12:17 +0530

    Time: 80 ms (88.14%), Space: 63 MB (59.89%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -12,6 +12,8 @@
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         if(root==NULL) return 0;
         int left=rangeSumBST(root->left,low,high);
         int right=rangeSumBST(root->right,low,high);
