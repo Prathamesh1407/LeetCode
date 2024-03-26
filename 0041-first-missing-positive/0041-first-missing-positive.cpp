351cf96a87efaca6c28e3334101aef03584381e9
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-03-26 21:22:39 +0530

    Time: 26 ms (98.33%), Space: 43.2 MB (99.44%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int n=nums.size();
         for(int i=0;i<n;i++) if(nums[i]<0) nums[i]=0;
         
