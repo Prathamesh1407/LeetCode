d9e748775ae3f51ddc5f9c2241b8d76d84c65dd7
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-03-29 08:46:57 +0530

    Time: 64 ms (99.81%), Space: 120.4 MB (49.61%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
         //unoredered_map<int,int>m;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int freq=0;
         int left=0,right=0;
         long long res=0;
