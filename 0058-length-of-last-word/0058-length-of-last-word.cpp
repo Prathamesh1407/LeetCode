cfbba0073649d990f16a1bd64e13d000b13b2ae3
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-03-31 13:59:47 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (57.58%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int c=0,i=s.length()-1;
         while(s[i]==' ')
         {
