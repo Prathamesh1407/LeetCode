43718b2b7f3410979ee759c5d167ea77991c33f2
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-03-31 14:34:01 +0530

    Time: 0 ms (100.00%), Space: 10.9 MB (72.37%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         if(strs.size()==0) return "";
         string prefix=strs[0];
         
