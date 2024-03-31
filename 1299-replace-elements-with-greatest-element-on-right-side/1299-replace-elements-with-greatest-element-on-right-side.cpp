86722980f28a99c82819fdbee30c4d3d28308cd4
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-03-31 12:37:49 +0530

    Time: 38 ms (99.46%), Space: 71 MB (29.13%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int mx=-1;
         for(int i=arr.size()-1;i>=0;i--)
         {
