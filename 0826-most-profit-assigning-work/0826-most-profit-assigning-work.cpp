c2926b9f6ce2054a3699669a95ef66b01225cacc
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-06-18 11:41:53 +0530

    Time: 54 ms (50.26%), Space: 42.4 MB (28.09%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        vector<pair<int,int>>v;
+        for(int i=0;i<profit.size();i++)
+        {
+            v.push_back({difficulty[i],profit[i]});
+        }
+        sort(v.begin(),v.end());
+        
+        sort(worker.begin(),worker.end());
+        
+        int i=0,j=0,ans=0;
+        priority_queue<int>pq;
+        while(j<worker.size())
+        {
+            
+            while(i<profit.size() && worker[j]>=v[i].first)
+            {
+                pq.push(v[i].second);
+                i++;
+            }
+            if(!pq.empty()) ans+=pq.top();
+            j++;
+        }
+        return ans;
+    }
+};
