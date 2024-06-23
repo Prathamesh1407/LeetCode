8dee3ddbe863f5b5fec86e110c6997b9caf83b84
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-06-23 08:33:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Swap two numbers/swap-two-numbers.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial template for c++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    pair<int, int> get(int a, int b){
+        //complete the function here
+        /*
+            a=2 b=3
+            a=2^3
+            b=(2^3)^3 = 2
+            a=(2^3)^2 = 3
+        */
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        return {a,b};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        
+        Solution ob;
+        pair<int, int>p = ob.get(a, b);
+        cout << p.first << ' ' << p.second << endl;
+    
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
