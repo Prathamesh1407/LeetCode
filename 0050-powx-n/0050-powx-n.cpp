c4197930f0056bc6ff1004e95b76466a3fe5e244
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-06-25 09:45:36 +0530

    Time: 3 ms (37.26%), Space: 7.9 MB (93.50%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,23 @@
+//TC : O(log2n)
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        int m=n;
+        n=abs(n);
+        while(n>0)
+        {
+            if(n&1) 
+            {
+                ans*=x;
+                n=n-1;
+            }
+            else
+            {
+                n/=2;
+                x=x*x;
+            }
+        }
+        return m>=0?ans:1/ans;
+    }
+};
