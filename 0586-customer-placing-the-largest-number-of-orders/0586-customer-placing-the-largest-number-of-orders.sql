284d7f1b5ef0c64963cf8b09d5579691d4a496d0
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-06-27 23:25:16 +0530

    Time: 439 ms (85.67%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -3,3 +3,15 @@ select customer_number from orders
 group by customer_number
 order by count(customer_number) desc 
 limit 1;
+
+#Follow up
+# SELECT customer_number
+# FROM orders
+# GROUP BY customer_number
+# HAVING COUNT(order_number) = (
+# 	SELECT COUNT(order_number) cnt
+# 	FROM orders
+# 	GROUP BY customer_number
+# 	ORDER BY cnt DESC
+# 	LIMIT 1
+# )
