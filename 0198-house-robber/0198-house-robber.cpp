6834135670d821dbbbdadb7d1230accc67539379
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-01 19:22:48 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (34.99%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -16,6 +16,21 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
-        return solve(nums,n-1,dp);
+        //Memoization
+        //return solve(nums,n-1,dp);
+        
+        //Tabulation
+        dp[0]=nums[0];
+        for(int ind=1;ind<n;ind++)
+        {
+            int pick=nums[ind];
+            if(ind>1) pick+=dp[ind-2];
+        
+            //Not pick
+            int notPick=0+dp[ind-1];
+
+            dp[ind]=max(pick,notPick);
+        }
+        return dp[n-1];
     }
 };
