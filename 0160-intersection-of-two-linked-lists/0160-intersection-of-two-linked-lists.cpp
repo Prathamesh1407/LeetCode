663a1058b1625dab6083ee42fa552050fd0492fd
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-02 23:47:18 +0530

    Time: 44 ms (32.89%), Space: 17.1 MB (70.00%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -6,8 +6,13 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+//TC : O(N1 + N2)
+//SC : O(1)
 class Solution {
 public:
+    
+    //If lets say N1 is lesser than N2 so move both the pointers simultaneously when the N1 will reach NULL exactly the N2 list will be on N2-N1 node then N1 will go to headB and it will cross that N2-N1 distance and then N2 will reach NULL and then to headA , now both pointers will align and we can find the 1st common node
+    
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode* a=headA;
         ListNode* b=headB;
