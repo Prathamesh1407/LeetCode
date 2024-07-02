fc6cc1f1660a6c1e7f64356dae517d8f267f385f
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-02 14:04:07 +0530

    Time: 7 ms (19.22%), Space: 13.4 MB (20.29%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,37 +8,43 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//Here the fast pointer is moved over length
+//TC : O(len)
+//TC : O(1)
 class Solution {
-    int getLength(ListNode* head)
-    {
-        int c=0;
-        while(head!=NULL)
-        {
-            c++;
-            head=head->next;
-        }
-        return c;
-    }
+    
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        int l=getLength(head);
-        n=l-n+1;
-        ListNode* curr=head,*dummyNode=new ListNode(-1),*prev=dummyNode;
-        dummyNode->next=head;
-        int i=1;
-        while(true)
+        
+        //Here we will be using the approach of Tortoise and hare move the fast pointer N times and then move the slow and fast by 1,so the fast already moves 2 times ahead of head the slow will reach to length-2 , so in this approach we won't need the length which was taking the O(len) extra
+        
+        ListNode *fast=head,*slow=head;
+        for(int i=0;i<n;i++) 
         {
-            if(i==n)
-            {
-                prev->next=curr->next;
-                break;
-            }
-            i++;
-            prev=curr;
-            curr=curr->next;
+            fast=fast->next;
         }
-        return dummyNode->next;
+        
+        //If fast is NULL that means the N = Length so we have to delete the head
+        if(fast==NULL)
+        {
+            ListNode *newHead=head->next;
+            delete(head);
+            return newHead;
+        }
+        
+        while(fast->next!=NULL) //If fast at the last node then slow will be on the previous node of length-N
+        {
+            slow=slow->next;
+            fast=fast->next;
+        }
+        
+        ListNode* NodeToBeDeleted=slow->next;
+        
+        slow->next=slow->next->next;
+        delete NodeToBeDeleted;
+        return head;
     }
 };
