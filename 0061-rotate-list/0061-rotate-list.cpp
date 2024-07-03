c832d7371715523e7bedad8bd2d44f7a2791cbab
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-03 13:12:27 +0530

    Time: 6 ms (40.88%), Space: 15.2 MB (26.57%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -8,40 +8,43 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//TC : O(2N)
+//SC : O(1)
 class Solution {
-    int getLength(ListNode* head)
-    {
-        int c=0;
-        while(head!=NULL)
-        {
-            c++;
-            head=head->next;
-        }
-        return c;
-    }
+    
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
+        
         if(head==NULL || k==0 || head->next==NULL) return head;
-        int l=getLength(head);
-        k=k%l;
-        if(k==0) return head;
-        ListNode*curr=head,*p,*m;
-        k=l-k;
-        int i=1;
-        while(curr->next)
+        
+        
+        int len=1;
+        ListNode*tail=head;
+        while(tail->next!=NULL)
         {
-            if(i==k)
-            {
-                p=curr;
-            }
-            i++;
-            curr=curr->next;
+            len++;
+            tail=tail->next;
         }
-        m=p->next;
-        p->next=NULL;
-        curr->next=head;
-        return m;
+
+        k=k%len;
+        if(k==0) return head;
+        
+        tail->next=head;
+        k=len-k;
+        
+        ListNode*KthNode=tail;
+        //Now let's get the Kth Node
+        
+        while(k--)
+        {
+            KthNode=KthNode->next;
+        }
+        cout<<KthNode->val;
+        head=KthNode->next;
+        KthNode->next=NULL;
+        return head;
     }
+
 };
