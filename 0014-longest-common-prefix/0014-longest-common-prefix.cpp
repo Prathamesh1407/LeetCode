917bd4db4c2076c3d8a4ee2b39265f6db6ca0d7c
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-04 13:32:18 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (34.84%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -16,6 +16,8 @@ public:
         }
         return prefix;
         */
+        
+        /*
         if(strs.size()==0) return "";
         string prefix=strs[0];
         
@@ -32,5 +34,19 @@ public:
             if(prefix=="") return prefix;
         }
         return prefix;
+        */
+        
+        //nlogn + min(first.length(),last.length())
+        if(strs.size()==0) return "";
+        sort(strs.begin(),strs.end());
+        string first=strs[0];
+        string last=strs[strs.size()-1];
+        string ans="";
+        for(int i=0;i<min(first.length(),last.length());i++)
+        {
+            if(first[i]!=last[i]) return ans;
+            else ans+=first[i];
+        }
+        return ans;
     }
 };
