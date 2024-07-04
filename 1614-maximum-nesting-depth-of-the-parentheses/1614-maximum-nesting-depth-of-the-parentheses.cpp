437feddfa5081c52999ad358c9a9bfc2852e562c
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-04 19:44:52 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (96.59%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxDepth(string s) {
+        //TC : O(N)
         int c=0,maxi=0;
         for(auto val:s)
         {
@@ -9,5 +10,7 @@ public:
             maxi=max(maxi,c);
         }
         return maxi;
+        
+        //Another approach can be using stack if ( push in the stack and if ) check the stack size and keep updating max by size
     }
 };
