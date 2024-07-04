44fde1da83f1e6d11e9a82da382f39bcd9f2de6a
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-04 18:11:52 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (62.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        //A1 : Sort the 2 string and if any char differs return false TC : O(nlogn)
+        
+        //TC:O(n) SC : O(1)
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         vector<int>v(26,0);
