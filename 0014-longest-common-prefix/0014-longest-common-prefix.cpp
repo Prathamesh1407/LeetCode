6c3bc53b5e5aae2b3172dc5dee1b11af3bef4596
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-04 13:05:10 +0530

    Time: 7 ms (28.21%), Space: 11.6 MB (12.22%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,8 +1,20 @@
 class Solution {
+    string getCommonPart(string s1,string s2)
+    {
+        int n=min(s1.length(),s2.length());
+        string ans="";
+        for(int i=0;i<n;i++)
+        {
+            if(s1[i]!=s2[i]) return ans;
+            else ans+=s1[i];
+        }
+        return ans;
+    }
 public:
     string longestCommonPrefix(vector<string>& strs) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
+        /*
         if(strs.size()==0) return "";
         string prefix=strs[0];
         
@@ -14,5 +26,15 @@ public:
             }
         }
         return prefix;
+        */
+        if(strs.size()==0) return "";
+        string prefix=strs[0];
+        
+        for(int i=1;i<strs.size();i++)
+        {
+            prefix=getCommonPart(prefix,strs[i]);
+        }
+        return prefix;
+        
     }
 };
