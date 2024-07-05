8187324db4277c14fe8db81bf3b3b4fca190052b
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-05 19:45:59 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (7.46%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
+        //TC :O(N) 
+        //SC :O(1)
+        /*
         int c=0,ans=0;
         for(auto val:s)
         {
@@ -13,5 +16,29 @@ public:
             }
         }
         return ans+c;
+        */
+        
+        stack<int>st;
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if(!st.empty())
+                {
+                    if(st.top()=='(') st.pop();
+                    else 
+                    {
+                        st.push(')');
+                    }
+                }
+                else st.push(')');
+            }
+        }
+        return st.size();
     }
 };
