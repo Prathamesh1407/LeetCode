2b2d80aefb9a7d027e3a80027f26a345797f06bf
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-08 23:16:15 +0530

    Update 0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,7 +9,6 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//T.C : O(n*logn)
 class Solution {
     int findPos(vector<int>& in,int s,int e,int x)
     {
@@ -35,10 +34,42 @@ class Solution {
         
         return root;
     }
+    TreeNode* solve2(vector<int>& preorder, vector<int>& inorder,int &ind,int s,int e,unordered_map<int,int>&m)
+    {
+        if(s>e || ind>=preorder.size()) return NULL;
+        
+        int element=preorder[ind];
+        ind++;
+        TreeNode* root=new TreeNode(element);
+        int indInInorder=m[element];
+        
+        root->left=solve2(preorder,inorder,ind,s,indInInorder-1,m);
+        root->right=solve2(preorder,inorder,ind,indInInorder+1,e,m);
+        
+        return root;
+    }
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        
+        //TC : O(N*logN) coz we are searching in the range of s and e so it's getting divide by 2 every time so log N
+        //SC : O(N)
+        //A1: Solve this by finding the index of preorder element in the inorder
+        
+        // int pre=0;
+        // TreeNode* ans=solve(preorder,inorder,pre,0,inorder.size()-1);
+        // return ans;
+        
+        
+        //TC : O(N)
+        //SC : O(N)
+        //A2: Instead of finding the index of preorder element in the inorder store it in the map and access it in constant time
         int pre=0;
-        TreeNode* ans=solve(preorder,inorder,pre,0,inorder.size()-1);
+        unordered_map<int,int>m;
+        for(int i=0;i<inorder.size();i++)
+        {
+            m[inorder[i]]=i;
+        }
+        TreeNode* ans=solve2(preorder,inorder,pre,0,inorder.size()-1,m);
         return ans;
     }
 };
