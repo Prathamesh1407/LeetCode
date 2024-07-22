4c027c4ca84c7c64e61e6540591a53e848b2a9bb
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-22 17:57:21 +0530

    Time: 425 ms (73.07%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
-# select id,if(p_id is null,'Root',if(count(id)=2,'Inner','Leaf')) as type from Tree;
 
 select distinct t1.id,if(t1.p_id is null,'Root',if(t2.p_id is not null,'Inner','Leaf')) as type from Tree t1 left join Tree t2 on t1.id=t2.p_id;
