7cd8184cb8450ca057260f80dd4b0cc9fc500dff
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-22 18:32:50 +0530

    Time: 106 ms (85.47%), Space: 69.3 MB (11.40%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    bool possible(vector<int>&bloomDay,int day,int m,int k)
+    {
+        int c=0,ans=0;
+        for(auto val:bloomDay)
+        {
+            if(val<=day)
+            {
+                //Bloom adjacent
+                c++;
+            }
+            else
+            {
+                //Not adjacent
+                ans+=(c/k);//count the no. of bouque could be made
+                c=0;
+            }
+        }
+        ans+=(c/k);
+        return ans>=m;
+    }
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        //The flowers will bloom only from the minimum to maximum element days so this is our search space
+        if(1LL*m*k>bloomDay.size()) return -1;
+        int maxi,mini;
+        maxi=mini=bloomDay[0];
+        for(auto val:bloomDay)
+        {
+            maxi=max(maxi,val);
+            mini=min(mini,val);
+        }
+        int low=mini,high=maxi;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(possible(bloomDay,mid,m,k))
+            {
+                //we want minimum so search it on the left side
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return low;
+    }
+};
