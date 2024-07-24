f5136a9602a779e699311da8c66a71c508cc4079
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-24 21:36:23 +0530

    Time: 3 ms (66.40%), Space: 12.2 MB (8.53%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& a, int key) {
+        
+        //TC : O(N * log2(M))
+        
+        //A1: every row is sorted so check the 1st and last element of each row by comparing target and if satisfies apply binary search on that row
+        
+        
+        //TC : O(log2(N*M))
+        int row=a.size(),col=a[0].size();
+        int s=0,e=row*col-1,mid;
+        while(s<=e)
+        {
+            mid=s+(e-s)/2;
+            int rowInd=mid/col,colInd=mid%col;
+            int element=a[rowInd][colInd];
+            if(element==key) 
+            {
+                return 1;
+            }
+            if(element>key) e=mid-1;
+            else s=mid+1;
+        }
+        return 0;
+    }
+};
