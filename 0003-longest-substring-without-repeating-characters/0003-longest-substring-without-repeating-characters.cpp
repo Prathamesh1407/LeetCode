dd917fd51209d9c0326e9cea385de5a613346438
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-25 10:28:38 +0530

    Time: 0 ms (100.00%), Space: 11.3 MB (53.46%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,6 +3,11 @@ public:
     int lengthOfLongestSubstring(string s) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
+        
+        //A1:
+        //TC : O(2*N) sometimes when element existed in substring already it has to traversed to that next existing element
+        //SC : O(1)
+        /*
         unordered_set<char>st;
         int left=0,right=0,res=0;
         while(right<s.length())
@@ -17,5 +22,33 @@ public:
             right++;
         }
         return res;
+        */
+        
+        //A2
+        //TC : O(2*N) sometimes when element existed in substring already it has to traversed to that next existing element
+        //SC : O(1)
+        
+        vector<int>v(256,-1);
+        
+        int left=0,right=0,res=0,n=s.length();
+        
+        while(right<n)
+        {
+            if(v[s[right]]!=-1)
+            {
+                if(v[s[right]]>=left)
+                {
+                    //Means present in the current substring not the previous one then bring that left pointer to that previously existing index + 1
+                    left=v[s[right]]+1;
+                    //cout<<left<<" ";
+                }
+            }
+            
+            res=max(res,right-left+1);
+            v[s[right]]=right;
+            right++;
+        }
+        return res;
+        
     }
 };
