c43ed46f64b03e4d570a2f39ab361568c42436ca
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-30 11:28:02 +0530

    Time: 104 ms (40.03%), Space: 38.4 MB (37.32%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -20,8 +20,12 @@ public:
         return ans;
         */
         
-        //TC : O(N)
+        //TC : O(3N)
         //SC : O(2N)
+        
+        //What we don't want is b at the right side and a in the left side so precompute the counts and find the minimum of their addition
+        
+        /*
         int n=s.length(),ans=INT_MAX;
         vector<int>rightA(n);
         vector<int>leftB(n);
@@ -45,5 +49,30 @@ public:
             ans=min(ans,leftB[i]+rightA[i]);
         }
         return ans;
+        */
+        
+        //TC : O(2N)
+        //SC : O(N)
+        //Instead of taking 2 vectors just take only 1 because at the 3rd loop we are starting from the start only so there is no need to precompute the B 
+        
+        int n=s.length(),ans=INT_MAX;
+        vector<int>rightA(n);
+        
+        int countB=0;
+        int countA=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            rightA[i]=countA;
+            if(s[i]=='a') countA++;
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            ans=min(ans,countB+rightA[i]);
+            if(s[i]=='b') countB++;
+        }
+        return ans;
+        
+        
     }
 };
