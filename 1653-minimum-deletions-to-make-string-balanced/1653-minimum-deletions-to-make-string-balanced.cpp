9fe26bf2c0a452ff9a7814c061dba6f9648327c4
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-07-30 11:22:01 +0530

    Time: 110 ms (31.74%), Space: 53.3 MB (13.08%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -4,6 +4,7 @@ public:
         
         //TC : O(N)
         //SC : O(N)
+        /*
         stack<char>st;
         int i=0,ans=0;
         
@@ -15,6 +16,33 @@ public:
                 ans++;
             }
             else st.push(s[i]);
+        }
+        return ans;
+        */
+        
+        //TC : O(N)
+        //SC : O(2N)
+        int n=s.length(),ans=INT_MAX;
+        vector<int>rightA(n);
+        vector<int>leftB(n);
+        
+        int countB=0;
+        for(int i=0;i<n;i++)
+        {
+            leftB[i]=countB;
+            if(s[i]=='b') countB++;
+        }
+        
+        int countA=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            rightA[i]=countA;
+            if(s[i]=='a') countA++;
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            ans=min(ans,leftB[i]+rightA[i]);
         }
         return ans;
     }
