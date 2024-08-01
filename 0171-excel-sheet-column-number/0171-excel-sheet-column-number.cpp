7395225e260388b770135f3626f73513a5447836
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-08-01 11:00:51 +0530

    Time: 2 ms (42.08%), Space: 8.9 MB (6.07%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int titleToNumber(string s) {
-        long long n=s.length(),ans=0;  
+        long long n=s.length(),ans=0;
+        //A->Z 26 AB whole 26 + 1 27 A B->z => BA means 2 traversal of 26 and 1 A : 53
         for(int i=0;i<n-1;i++)
         {
             ans+=(pow(26,n-i-1)*(s[i]-'A'+1));
