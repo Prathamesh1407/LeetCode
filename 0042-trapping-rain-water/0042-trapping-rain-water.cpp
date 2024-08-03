03fa3beeab03b6dc3c7262b3e6326af4a389a290
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-08-03 22:04:10 +0530

    Time: 7 ms (87.25%), Space: 22.6 MB (61.96%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        /*
+        //1st Approach
+        //TC : O(2N)
+        //SC : O(2N)
         int n=height.size();
         vector<int>pre,suff(n);
         int maxiPre=-1,maxiSuff=-1;
@@ -18,5 +22,29 @@ public:
             //cout<<suff[i]<<" ";
         }
         return c;
+        */
+        
+        //2ndApproach : 2-pointer Approach
+        //TC : O(N)
+        //SC : O(1)
+        int l=0,n=height.size(),r=n-1,ans=0;
+        int leftMax=0,rightMax=0;
+        while(l<=r)
+        {
+            if(height[l]<height[r])
+            {
+                if(height[l]>=leftMax) leftMax=height[l];
+                else ans+=leftMax-height[l];
+                l++;
+            }
+            else
+            {
+                //height[l]>=height[r]
+                if(height[r]>=rightMax) rightMax=height[r];
+                else ans+=rightMax-height[r];
+                r--;
+            }
+        }
+        return ans;
     }
 };
