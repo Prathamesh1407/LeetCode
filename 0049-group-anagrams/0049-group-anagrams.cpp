240c51a75f7a8d92f7b220c33b0134da2c51f41c
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-08-04 18:53:00 +0530

    Time: 36 ms (25.18%), Space: 28.9 MB (14.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,6 +3,10 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
+        
+        //TC : O(N * KlogK) K = Avg length Word
+        //SC : O(N)
+        /*
         unordered_map<string,vector<string>>m;
         vector<vector<string>>ans;
         for(auto val:strs)
@@ -12,6 +16,36 @@ public:
             m[s].push_back(val);
             
         }
+        for(auto val:m)
+        {
+            ans.push_back(val.second);
+        }
+        return ans;
+        */
+        
+        
+        //TC : O(N*K)
+        
+        
+        unordered_map<string,vector<string>>m;
+        for(auto val:strs)
+        {
+            vector<int>v(26,0);
+            for(auto ch:val)
+            {
+                v[ch-'a']++;
+            }
+            string s;
+            for(int i=0;i<26;i++)
+            {
+                if(v[i]!=0)
+                {
+                    s+=string(v[i],97+i);
+                }
+            }
+            m[s].push_back(val);
+        }
+        vector<vector<string>>ans;
         for(auto val:m)
         {
             ans.push_back(val.second);
