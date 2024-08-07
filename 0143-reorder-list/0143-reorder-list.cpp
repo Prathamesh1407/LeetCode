f60778e9e65ffda867b5e45ae82f140c128706da
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-08-07 15:23:29 +0530

    Time: 34 ms (13.40%), Space: 22.5 MB (48.18%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -9,29 +9,47 @@
  * };
  */
 class Solution {
+    ListNode* findMiddle(ListNode* head)
+    {
+        ListNode* fast=head,*slow=head;
+        
+        while(fast && fast->next)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow;
+    }
+    ListNode* reverseLL(ListNode* head)
+    {
+        ListNode* curr=head,*prev=NULL,*forward=head;
+        
+        while(curr!=NULL)
+        {
+            forward=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
+        }
+        return prev;
+    }
     public:
     void reorderList(ListNode* head) {
-        ListNode*h=head;
-        vector<ListNode*>v;
-        while(h)
+        if(head==NULL || head->next==NULL) return;
+        ListNode* middleNode=findMiddle(head);
+        
+        ListNode* head2=middleNode->next;
+        middleNode->next=NULL;
+        head2=reverseLL(head2);
+        ListNode* curr=head;
+        while(curr && head2)
         {
-            v.push_back(h);
-            h=h->next;
+            ListNode* temp=head2->next;
+            head2->next=curr->next;
+            curr->next=head2;
+            
+            curr=head2->next;
+            head2=temp;
         }
-        int i=0,j=v.size()-1;
-        head=v[i];
-        head->next=v[j];
-        h=head->next;
-        i++; 
-        while(j>i)
-        {
-            h->next=v[i];
-            h=h->next;
-            j--;
-            h->next=v[j];
-            h=h->next;
-            i++;
-        }
-        h->next=NULL;
     }
 };
