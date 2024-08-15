ea0d78a7875b20e9dc90fb6d3cf32704b44b5ad9
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-08-15 22:01:20 +0530

    Time: 105 ms (28.95%), Space: 43.9 MB (19.96%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -0,0 +1,31 @@
+class MyCalendar {
+public:
+    set<pair<int,int>>s;
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        if(s.size()==0) 
+        {
+            s.insert({start,end});
+            return 1;
+        }
+        
+        for(auto val:s)
+        {
+            if((val.first<=start && val.second>start) || (start<=val.first && end>val.first)) 
+            {
+                return 0; 
+            }
+        }
+        s.insert({start,end});
+        return 1;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
