f72ac5d340f4ed969a15b67f2346f4ef6b5d7731
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-11-24 13:20:23 +0530

    Time: 367 ms (96.47%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -6,5 +6,20 @@
 
 #2nd Approach
 
-select w2.id from Weather w1 join Weather w2
-on datediff(w2.recordDate,w1.recordDate)=1 AND w2.temperature>w1.temperature;
+# select w2.id from Weather w1 join Weather w2
+# on datediff(w2.recordDate,w1.recordDate)=1 AND w2.temperature>w1.temperature;
+
+WITH q1 AS (
+  SELECT 
+      *,
+      LAG(temperature) OVER (ORDER BY recordDate) AS previous_day_temperature,
+      LAG(recordDate) OVER (ORDER BY recordDate) AS previous_Date
+  FROM Weather
+)
+
+-- Select the IDs where the temperature is higher than the previous day
+-- and the previous day exists (difference is exactly 1 day)
+SELECT id
+FROM q1
+WHERE temperature > previous_day_temperature
+AND DATEDIFF(recordDate, previous_Date) = 1;
