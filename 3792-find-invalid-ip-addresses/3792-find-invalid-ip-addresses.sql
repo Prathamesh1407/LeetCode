3829dfcde3162b4be726e53fd4ce91a83cecce5a
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2025-02-27 16:16:56 +0530

    3792-FIND-INVALID-IP-ADDRESSES Stats: Time: 498 ms (55.56%), Space: 0B (100%) - LeetHub

--- 3792-find-invalid-ip-addresses/3792-find-invalid-ip-addresses.sql
+++ 3792-find-invalid-ip-addresses/3792-find-invalid-ip-addresses.sql
@@ -14,6 +14,6 @@
 
 SELECT ip,count(ip) as invalid_count
 FROM logs 
-WHERE ip NOT REGEXP '^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)$'
+WHERE ip NOT REGEXP '^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?)$'
 GROUP BY ip
 ORDER BY invalid_count DESC, ip DESC;
