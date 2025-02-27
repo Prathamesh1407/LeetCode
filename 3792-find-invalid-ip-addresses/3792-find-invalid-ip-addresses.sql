e92b39a7ceb879b9d9284d50e09e261a0b6f352d
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2025-02-27 16:19:52 +0530

    3792-FIND-INVALID-IP-ADDRESSES Stats: Time: 650 ms (22.47%), Space: 0B (100%) - LeetHub

--- 3792-find-invalid-ip-addresses/3792-find-invalid-ip-addresses.sql
+++ 3792-find-invalid-ip-addresses/3792-find-invalid-ip-addresses.sql
@@ -1,19 +1,5 @@
--- # Write your MySQL query statement below
--- SELECT ip, COUNT(*) AS invalid_count
--- FROM logs
--- WHERE ip NOT REGEXP 
--- '^(([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
--- GROUP BY ip
--- ORDER BY invalid_count DESC, ip DESC;
-
-
--- SELECT ip,count(ip) as invalid_count 
--- FROM logs WHERE ip not regexp '^(([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
--- GROUP BY ip
--- ORDER BY invalid_count DESC, ip DESC;
-
 SELECT ip,count(ip) as invalid_count
 FROM logs 
-WHERE ip NOT REGEXP '^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?)$'
+WHERE ip NOT REGEXP '^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?)$'
 GROUP BY ip
 ORDER BY invalid_count DESC, ip DESC;
