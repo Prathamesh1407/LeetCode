0ab7284f05e68dede175c3415b11c4d502b6dfde
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2025-04-23 03:00:16 +0530

    2524-LARGEST-POSITIVE-INTEGER-THAT-EXISTS-WITH-ITS-NEGATIVE Stats: Time: 3 ms (79.25%), Space: 27.4 MB (5.28%) - LeetHub

--- 2524-largest-positive-integer-that-exists-with-its-negative/2524-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/2524-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
+        /*
         unordered_map<int,int>m;
         int maxi=INT_MIN;
         for(auto val:nums)
@@ -13,5 +14,31 @@ public:
             }
         }
         return maxi==INT_MIN?-1:maxi;
+        */
+
+        // A2: As constrains are less we can directly use array
+
+        /*
+            Now numbers can be -ve how to index them
+            -1000 <= nums[i] <= 1000
+            total 2001 elements
+            add -ve index with highest limit here 1000
+            -1000 + 1000 = 0 so -1000 will be placed at 0 index
+            if -3 we want to find its counterpart then in array -(-3) = 3 1000 + 3 = 1003 th index will be -3's counterpart
+        */
+
+        vector<int>arr(2001,0);
+        int maxi=-1;
+        for(auto val:nums)
+        {
+            if(arr[-val+1000]==1)
+            {
+                //got counterpart 
+                maxi=max(maxi,abs(val));
+            }
+            arr[val+1000]=1;
+
+        }
+        return maxi;
     }
 };
