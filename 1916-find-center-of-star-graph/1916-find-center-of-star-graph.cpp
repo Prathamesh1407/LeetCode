0717cfaebb05e0198cab98fa33c3e848126007a9
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2025-05-04 12:12:02 +0530

    1916-FIND-CENTER-OF-STAR-GRAPH Stats: Time: 0 ms (100%), Space: 99 MB (87.67%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
+
+        //A1:
+        /*
         unordered_map<int,vector<int>>m;
 
         for(auto &val:edges)
@@ -17,6 +20,13 @@ public:
             if(val.second.size()==edges.size()) return val.first;
         }
 
+        return -1;
+        */
+
+        if(edges[0][1]==edges[1][0]) return edges[1][0];
+        if(edges[0][1]==edges[1][1]) return edges[1][1];
+        if(edges[0][0]==edges[1][0]) return edges[0][0]; 
+        if(edges[0][0]==edges[1][1]) return edges[0][0];
         return -1;
     }
 };
