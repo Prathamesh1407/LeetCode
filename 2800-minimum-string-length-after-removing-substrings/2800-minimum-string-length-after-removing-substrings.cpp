7536a5d83516c5c3d66be5a4c82dfdbdb97c5c99
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2025-05-06 16:53:51 +0530

    2800-MINIMUM-STRING-LENGTH-AFTER-REMOVING-SUBSTRINGS Stats: Time: 0 ms (100%), Space: 10.3 MB (84.29%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minLength(string st) {
         //A1: TC:O(n) SC: O(n) Use Stack
-
+        /*
         stack<char>s;
 
         for(auto val:st)
@@ -13,5 +13,33 @@ public:
             else s.push(val);
         }
         return s.size();
+        */
+
+        //A2: TC: O(n) SC:O(1) keep the 2 pointers i and j for writing and traversing resp. i will store the letters which are not AB or CD and between them so value of i will be our answer as length and j to check C and D
+
+        int i=0,j=1;
+        string s=st;
+        int n=s.length();
+        while(j<n)
+        {
+            if(i<0)
+            {
+                //if its get -1 write whatever j has
+                i++;
+                s[i]=s[j];
+            }
+            else if((s[i]=='A' && s[j]=='B') || (s[i]=='C' && s[j]=='D'))
+            {
+                //Do not want that
+                i--;
+            }
+            else
+            {
+                i++;
+                s[i]=s[j];
+            }
+            j++;
+        }
+        return i+1;
     }
 };
