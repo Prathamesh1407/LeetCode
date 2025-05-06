e92c0c119301427642c24fc0a6f55def1ab4716c
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2025-05-06 12:57:12 +0530

    2132-CONVERT-1D-ARRAY-INTO-2D-ARRAY Stats: Time: 9 ms (71.63%), Space: 89.8 MB (95.75%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& org, int m, int n) {
         if(m*n!=org.size()) return {};
+
+        //A1: TC: O(n*m)
+        /*
         int size=org.size();
         vector<vector<int>>v(m,vector<int>(n));
         int k=0;
@@ -11,6 +14,25 @@ public:
             {
                 v[i][j]=org[k++];
             }
+        }
+        return v;
+        */
+
+        //A2: whenever we have to map 1D to 2D array use this
+        /*
+            row=i/col;
+            col=i%col;
+        */
+
+        vector<vector<int>>v(m,vector<int>(n));
+        int size=org.size(),row,col;
+
+        for(int i=0;i<size;i++)
+        {
+            row=i/n;
+            col=i%n;
+
+            v[row][col]=org[i];
         }
         return v;
         
