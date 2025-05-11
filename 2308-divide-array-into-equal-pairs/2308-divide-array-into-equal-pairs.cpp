4657536939ea08c7d3ebd64a1d97c12f7c4b04db
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2025-05-12 00:18:59 +0530

    2308-DIVIDE-ARRAY-INTO-EQUAL-PAIRS Stats: Time: 4 ms (43.27%), Space: 18.1 MB (32.13%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
+        //A1: TC:O(N) SC:O(distinct(nums[i]))
+        
         unordered_map<int,int>m;
         for(auto val:nums) m[val]++;
 
         for(auto val:m) if(val.second%2==1) return 0;
         return 1;
+        
+
+        //A2: take array of 500 mark all true and toggle it when element comes if in pair , it will again comes as true if not it will false,if any one element in array is false , we will return 0
     }
 };
