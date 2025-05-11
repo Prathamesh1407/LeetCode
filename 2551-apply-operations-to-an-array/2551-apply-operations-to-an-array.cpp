6b4817f6c6a79862612e562be4b328fea7db6c1e
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2025-05-11 16:53:10 +0530

    2551-APPLY-OPERATIONS-TO-AN-ARRAY Stats: Time: 0 ms (100%), Space: 13.3 MB (24.7%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -21,6 +21,7 @@ public:
         */
 
         //A2: Inplace TC:O(n) SC:O(1) 2 pass
+        /*
         int n=nums.size();
         for(int i=0;i<n-1;i++)
         {
@@ -41,6 +42,27 @@ public:
             }
         }
         return nums;
+        */
 
+        //A2: Inplace TC:O(n) SC:O(1) 1 pass
+        int n=nums.size(),j=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i+1<n && nums[i]!=0 && nums[i]==nums[i+1])
+            {
+                nums[i]*=2;
+                nums[i+1]=0;
+            }
+            if(nums[i]!=0)
+            {
+                if(i!=j)
+                {
+                    //means somewhere j is on 0
+                    swap(nums[i],nums[j]);
+                }
+                j++;
+            }
+        }
+        return nums;
     }
 };
