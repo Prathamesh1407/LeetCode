8d88429442c94d5f44652dd719d73dad414de8e4
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2025-05-11 16:33:31 +0530

    2551-APPLY-OPERATIONS-TO-AN-ARRAY Stats: Time: 0 ms (100%), Space: 13.2 MB (61.87%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
+
+        //A1: Approach 1 TC:O(n) SC:O(n)
+        /*
         vector<int>ans(nums.size(),0);
         int k=0;
 
@@ -15,5 +18,29 @@ public:
         }
         if(nums[nums.size()-1]!=0) ans[k]=nums[nums.size()-1];
         return ans;
+        */
+
+        //A2: Inplace TC:O(n) SC:O(1) 2 pass
+        int n=nums.size();
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i]!=0 && nums[i]==nums[i+1])
+            {
+                nums[i]*=2;
+                nums[i+1]=0;
+            }
+        }
+
+        int j=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=0) 
+            {
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+        return nums;
+
     }
 };
