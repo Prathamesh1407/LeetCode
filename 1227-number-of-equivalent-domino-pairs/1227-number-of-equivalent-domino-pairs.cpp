67b4e1cbbb4577cea90be71bc5008e13f3b28f0d
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2025-05-18 18:12:52 +0530

    1227-NUMBER-OF-EQUIVALENT-DOMINO-PAIRS Stats: Time: 12 ms (16.2%), Space: 28.6 MB (13.18%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        //A1:
+        /*
         unordered_map<string,int>m;
         int ans=0;
         for(auto val:dominoes)
@@ -11,6 +13,23 @@ public:
         for(auto val:m)
         {
             ans+=((val.second*(val.second-1))/2);
+        }
+        return ans;
+        */
+
+        //A2: Using unique answer trick a*10+b
+
+        vector<int>v(100,0);
+        int ans=0;
+        for(auto val:dominoes)
+        {
+            int mini=min(val[0],val[1]);
+            int maxi=max(val[0],val[1]);
+            v[mini*10+maxi]++;
+        }
+        for(auto val:v)
+        {
+            ans+=((val*(val-1))/2);
         }
         return ans;
     }
