05f3a14c23df1f6828955fc9eef8bfbf20144b42
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2025-05-22 17:44:53 +0530

    1586-LONGEST-SUBARRAY-OF-1S-AFTER-DELETING-ONE-ELEMENT Stats: Time: 0 ms (100%), Space: 60.2 MB (25.3%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
+        //A1:O(2n)
+        /*
         int zeros=0,maxi=0,streak=0;
         int i=0;
         for(int j=0;j<nums.size();j++)
@@ -10,6 +12,23 @@ public:
             {
                 if(nums[i]==0) zeros--;
                 i++;
+            }
+            maxi=max(maxi,j-i);
+        }
+        return maxi;
+        */
+
+        //What we were doing in last approach, traversing the i th pointer till next 0 so we will store the last 0 index and will directly jump to it
+
+        int zeros=0,maxi=0,streak=0,lastZeroInd=-1;
+        int i=0;
+        for(int j=0;j<nums.size();j++)
+        {
+            if(nums[j]==0) 
+            {
+                //if j encounters with 0 bring the ith pointer to the next of last 0 
+                i=lastZeroInd+1;
+                lastZeroInd=j;
             }
             maxi=max(maxi,j-i);
         }
